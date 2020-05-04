9fb4db5d499923986866976cdcca1a258c2614b7
Matthew Fishman <mfishman@flatironinstitute.org>
2020-05-04 17:15:41 -0400

    First commit (moved from ITensors.jl)

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+using Test
+
+@testset "NDTensors.jl" begin
+  @testset "$filename" for filename in (
+    "linearalgebra.jl",
+    "dense.jl",
+    "blocksparse.jl",
+    "diag.jl",
+  )
+    println("Running $filename")
+    include(filename)
+  end
+end
