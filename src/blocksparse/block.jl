47e1a726b07ad204e745a72dfdd6b45d2a812d26
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2021-01-12 15:29:56 -0500

    Add slicing for Block

--- src/blocksparse/block.jl
+++ src/blocksparse/block.jl
@@ -63,6 +63,8 @@ convert(::Type{Block{N}}, t::Tuple) where {N} = Block{N}(t)
 
 (::Type{IntT})(b::Block{1}) where {IntT <: Integer}= IntT(only(b))
 
+getindex(B::Block{N}, R::UnitRange{Int64}) where {N} = Block(B.data[R])
+
 #
 # Getting and setting fields
 #
